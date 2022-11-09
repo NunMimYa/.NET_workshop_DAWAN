76255c4ce332544215d80e2814ca6dbaba5ba1d2
Laurie JEZEQUEL <ljezequelpfl@hotmail.fr>
2022-11-09 11:06:30 +0100

    Add comments inside Course Entity

--- ContosoUniversity/Models/Course.cs
+++ ContosoUniversity/Models/Course.cs
@@ -16,10 +16,22 @@ namespace ContosoUniversity.Models
         [Range(0, 5)]
         public int Credits { get; set; }
 
+        /*
+         * Foreign key and navigation properties.
+         * The foreign key (FK) properties and navigation properties in the Course entity reflect the following relationships:
+         * A course is assigned to one department, so there's a DepartmentID FK and a Department navigation property.
+         */
+
         public int DepartmentID { get; set; }
 
         public Department Department { get; set; }
+        /*
+         * A course can have any number of students enrolled in it, so the Enrollments navigation property is a collection:
+         */
         public ICollection<Enrollment> Enrollments { get; set; }
+        /*
+         * A course may be taught by multiple instructors, so the Instructors navigation property is a collection:
+         */
         public ICollection<Instructor> Instructors { get; set; }
     }
 }
