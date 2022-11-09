37c02333f2af2d7b10bebd4358ca72dd68d55391
Laurie JEZEQUEL <ljezequelpfl@hotmail.fr>
2022-11-09 14:02:10 +0100

    Part6 - Display the department name

--- ContosoUniversity/Pages/Courses/Index.cshtml.cs
+++ ContosoUniversity/Pages/Courses/Index.cshtml.cs
@@ -19,15 +19,28 @@ namespace ContosoUniversity.Pages.Courses
             _context = context;
         }
 
-        public IList<Course> Course { get;set; } = default!;
+        //public IList<Course> Course { get;set; } = default!;
+        /* UPDATE */
+        public IList<Course> Courses { get; set; }
 
         public async Task OnGetAsync()
         {
             if (_context.Courses != null)
             {
-                Course = await _context.Courses
-                .Include(c => c.Department).ToListAsync();
+                //Course = await _context.Courses
+                //.Include(c => c.Department).ToListAsync();
+
+                Courses = await _context.Courses
+                .Include(c => c.Department)
+                .AsNoTracking()
+                .ToListAsync();
             }
         }
     }
 }
+
+/*
+ * The preceding code changes the Course property to Courses and adds AsNoTracking. 
+ * AsNoTracking improves performance because the entities returned are not tracked. 
+ * The entities don't need to be tracked because they're not updated in the current context.
+ */
