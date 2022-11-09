20dca2f27e69f808f53c4ca6398204f7483cb444
Laurie JEZEQUEL <ljezequelpfl@hotmail.fr>
2022-11-09 16:16:51 +0100

    Part7: Update Related Data - Create a class for assigned courses data

---
+++ ContosoUniversity/Models/SchoolViewModels/AssignedCourseData.cs
@@ -0,0 +1,14 @@
+﻿namespace ContosoUniversity.Models.SchoolViewModels
+{
+    public class AssignedCourseData
+    {
+        public int CourseID { get; set; }
+        public string Title { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
+
+/*
+ * The AssignedCourseData class contains data 
+ * to create the checkboxes for courses assigned to an instructor.
+ */
