7ec8a0b43853f0f7aeb9a83e81c84bf50d63c490
Laurie JEZEQUEL <ljezequelpfl@hotmail.fr>
2022-11-09 21:08:15 +0100

    Part8: Concurrency - Add a utility class

---
+++ ContosoUniversity/Utility.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * The Utility class provides the GetLastChars method used to display the last few characters of the concurrency token. 
+ * The following code shows the code that works with both SQLite ad SQL Server:
+ */
+
+#if SQLiteVersion
+using System;
+
+namespace ContosoUniversity
+{
+    public static class Utility
+    {
+        public static string GetLastChars(Guid token)
+        {
+            return token.ToString().Substring(
+                                    token.ToString().Length - 3);
+        }
+    }
+}
+#else
+namespace ContosoUniversity
+{
+    public static class Utility
+    {
+        public static string GetLastChars(byte[] token)
+        {
+            return token[7].ToString();
+        }
+    }
+}
+#endif
