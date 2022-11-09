a2b903bcbb58e2456e741128ec39cc79b78cda14
Laurie JEZEQUEL <ljezequelpfl@hotmail.fr>
2022-11-09 10:58:02 +0100

    OfficeAssignment entity

--- ContosoUniversity/Models/Instructor.cs
+++ ContosoUniversity/Models/Instructor.cs
@@ -1,6 +1,46 @@
-﻿namespace ContosoUniversity.Models
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics;
+using System.Security.Policy;
+
+namespace ContosoUniversity.Models
 {
     public class Instructor
     {
+        public int ID { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [Required]
+        [Column("FirstName")]
+        [Display(Name = "First Name")]
+        [StringLength(50)]
+        public string FirstMidName { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set; }
+
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get { return LastName + ", " + FirstMidName; }
+        }
+
+        //Navigation properties
+        //The Courses and OfficeAssignment properties are navigation properties.
+        //An instructor can teach any number of courses, so Courses is defined as a collection.
+        public ICollection<Course> Courses { get; set; }
+        //An instructor can have at most one office,
+        //so the OfficeAssignment property holds a single OfficeAssignment entity.
+        //OfficeAssignment is null if no office is assigned.
+        public OfficeAssignment OfficeAssignment { get; set; }
     }
 }
--- ContosoUniversity/Models/OfficeAssignment.cs
+++ ContosoUniversity/Models/OfficeAssignment.cs
@@ -1,6 +1,36 @@
-﻿namespace ContosoUniversity.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ContosoUniversity.Models
 {
     public class OfficeAssignment
     {
+        /*
+        The Key attribute
+        The [Key] attribute is used to identify a property as the primary key (PK) when the property name is something other than classnameID or ID.
+        There's a one-to-zero-or-one relationship between the Instructor and OfficeAssignment entities. An office assignment only exists in relation to the instructor it's assigned to. The OfficeAssignment PK is also its foreign key (FK) to the Instructor entity. A one-to-zero-or-one relationship occurs when a PK in one table is both a PK and a FK in another table.
+        EF Core can't automatically recognize InstructorID as the PK of OfficeAssignment because InstructorID doesn't follow the ID or classnameID naming convention. Therefore, the Key attribute is used to identify InstructorID as the PK:
+        */
+        [Key]
+        public int InstructorID { get; set; }
+        /*
+         * By default, EF Core treats the key as non-database-generated 
+         * because the column is for an identifying relationship
+         */
+        [StringLength(50)]
+        [Display(Name = "Office Location")]
+        public string Location { get; set; }
+        /*The Instructor navigation property
+         * The Instructor.OfficeAssignment navigation property can be null 
+         * because there might not be an OfficeAssignment row for a given instructor. 
+         * An instructor might not have an office assignment.
+         * The OfficeAssignment.Instructor navigation property will always have 
+         * an instructor entity because the foreign key InstructorID type is int, 
+         * a non-nullable value type. 
+         * An office assignment can't exist without an instructor.
+         * When an Instructor entity has a related OfficeAssignment entity, 
+         * each entity has a reference to the other one in its navigation property.
+         */
+        public Instructor Instructor { get; set; }
     }
 }
